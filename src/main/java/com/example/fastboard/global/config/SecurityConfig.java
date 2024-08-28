436af232add042fd32627f7d70cc8b6227978757
ChangHoMoon <tony9911@naver.com>
2024-08-28 16:59:57 +0900

    [feat] 시큐리티 적용
    - <SecurityConfig> 생성
      - 예외 핸들링
      - UsernamePasswordAuthenticationFilter 동작 전에 JwtFilter 수행
    
    - <JwtTokenProvider> 생성
      - AccessToken 생성 메소드
      - RefreshToekn 생성 메소드
      - Authentication 객체 get 메소드
      - AccessToken을 가지고 Claims를 추출하는 메소드
      - 토큰 유효성 검사 메소드
      - 헤더에서 AccessToken 추출 하는 메소드
    
    - <JwtAuthenticationFilter> 생성
      - `addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)` 로 UsernamePasswordAuthenticationFilter 전에 사용되어 대신 Authentication을 생성해서 시큐리티 콘텍스트에 인증 객체를 넣어주는 필터
      - flow : 클라리이언트 요청 헤더에서 token추출 -> 토큰이 유효하다면 Authentication 객체 생성 -> 컨텍스트 홀더에 주입
    
    - <CustomUserDetailsService>
      - username을 기반으로 사용자 정보를 로딩하는 역활
      - Member에서 사용자를 찾고 UserDetails객체로 만들어서 반환하는 역할

---
+++ src/main/java/com/example/fastboard/global/config/SecurityConfig.java
@@ -0,0 +1,96 @@
+package com.example.fastboard.global.config;
+
+import com.example.fastboard.global.common.ResponseDTO;
+import com.example.fastboard.global.config.jwt.JwtAuthenticationFilter;
+import com.example.fastboard.global.config.jwt.JwtTokenProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity(debug = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    private static final String[] ALLOWED_PATHS = {
+            "/h2-console/**",
+            "/boards",
+            "/boards/search",
+            "/members/signup",
+            "/members/login"
+    };
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .headers(headers ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .authorizeHttpRequests(authorize ->
+                        authorize
+                                .requestMatchers(
+                                        Arrays.stream(ALLOWED_PATHS)
+                                                .map(AntPathRequestMatcher::new)
+                                                .toArray(RequestMatcher[]::new)
+                                ).permitAll()
+                                .anyRequest().authenticated()
+                )
+
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .accessDeniedHandler(
+                                (request, response, accessDeniedException) -> {
+                                    ResponseDTO<Void> responseDTO = ResponseDTO.errorWithMessage(HttpStatus.FORBIDDEN, "Access Denied");
+                                    sendResponse(response, responseDTO);
+                                }
+                        )
+                        .authenticationEntryPoint(
+                                (request, response, accessDeniedException) -> {
+                                    ResponseDTO<Void> responseDTO = ResponseDTO.errorWithMessage(HttpStatus.UNAUTHORIZED, "Unauthorized: 1. 비로그인 상태로 로그인이 필요한 API에 접근했거나 2. 없는 API URI에 요청을 보내고 있습니다");
+                                    sendResponse(response, responseDTO);
+                                }
+                        )
+                )
+                .formLogin(AbstractHttpConfigurer::disable)
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    private void sendResponse(HttpServletResponse response, ResponseDTO<Void> responseDTO) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(responseDTO.getStatus());
+        PrintWriter out = response.getWriter();
+        ObjectMapper objectMapper = new ObjectMapper();
+        out.print(objectMapper.writeValueAsString(responseDTO));
+        out.flush();
+    }
+}
