fc6a5736f51158ddc83a0a0fd846581f0b3ba561
ChangHoMoon <tony9911@naver.com>
2024-09-20 16:28:21 +0900

    [feat] 유효성 검사 추가

--- src/main/java/com/example/fastboard/domain/board/controller/BoardController.java
+++ src/main/java/com/example/fastboard/domain/board/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.example.fastboard.domain.board.controller;
 import com.example.fastboard.domain.board.dto.request.BoardCreateRequest;
 import com.example.fastboard.domain.board.service.BoardService;
 import com.example.fastboard.global.common.ResponseDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,11 @@ public class BoardController {
 
     @PostMapping
     public ResponseEntity<ResponseDTO<Long>> create(
-            @RequestBody BoardCreateRequest request,
+            @Valid @RequestBody BoardCreateRequest request,
             Principal principal
     ) {
         Long memberId = Long.valueOf(principal.getName());
         Long boardId = boardService.create(request, memberId);
-
         return ResponseEntity.ok(ResponseDTO.okWithData(boardId));
     }
-
 }
--- src/main/java/com/example/fastboard/domain/board/dto/request/BoardCreateRequest.java
+++ src/main/java/com/example/fastboard/domain/board/dto/request/BoardCreateRequest.java
@@ -3,14 +3,18 @@ package com.example.fastboard.domain.board.dto.request;
 import com.example.fastboard.domain.board.entity.Board;
 import com.example.fastboard.domain.board.entity.Category;
 import com.example.fastboard.domain.member.entity.Member;
+import jakarta.validation.constraints.NotNull;
 
 public record BoardCreateRequest(
-    String title,
-    String content,
-    Category category
+        @NotNull(message = "제목을 입력해주세요")
+        String title,
+        @NotNull(message = "내용을 입력해주세요")
+        String content,
+        @NotNull(message = "카테고리를 선택해 주세요")
+        Category category
 ) {
 
-    public Board toEntity(Member member){
+    public Board toEntity(Member member) {
         return Board.builder()
                 .title(title)
                 .content(content)
