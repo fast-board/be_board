c0d936d52d2118b2117fe1be16b098de522fc7a4
공태현 <dev0taehyeon@gmail.com>
2024-10-28 17:30:02 +0900

    [refac] : Disable Cors

--- src/main/java/com/example/fastboard/FastBoardApplication.java
+++ src/main/java/com/example/fastboard/FastBoardApplication.java
@@ -2,8 +2,11 @@ package com.example.fastboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -14,4 +17,19 @@ public class FastBoardApplication {
 		SpringApplication.run(FastBoardApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("Authorization", "Content-Type")
+						.exposedHeaders("Custom-Header")
+						.allowCredentials(true)
+						.maxAge(3600);
+			}
+		};
+	}
 }
